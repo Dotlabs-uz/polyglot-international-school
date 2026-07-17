df1b7d4e8ce312e048d49a3af54fe02b69aca1a2
Sardor-web-dev <dzamolovsardor5@gmail.com>
2026-07-17 10:45:51 +0500

    fixed form

--- components/forms/AmoCrmForm.tsx
+++ components/forms/AmoCrmForm.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-import Script from "next/script";
+import { useEffect, useRef } from "react";
 
-const INIT_SCRIPT = `!function(a,m,o,c,r,m){a[o+c]=a[o+c]||{setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1725042",hash:"7d50db6eef500612d3f2c7e6bb64b14a",locale:"ru"}),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");`;
+const INIT = `!function(a,m,o,c,r,m){a[o+c]=a[o+c]||{setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1725042",hash:"7d50db6eef500612d3f2c7e6bb64b14a",locale:"ru"}),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");`;
 
 export function AmoCrmForm() {
-  return (
-    <>
-      <Script
-        id="amo-crm-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{ __html: INIT_SCRIPT }}
-      />
-      <Script
-        id="amoforms_script_1725042"
-        strategy="afterInteractive"
-        src="https://forms.amocrm.ru/forms/assets/js/amoforms.js?1784264472"
-      />
-    </>
-  );
+  const ref = useRef<HTMLDivElement>(null);
+  const done = useRef(false);
+
+  useEffect(() => {
+    if (done.current || !ref.current) return;
+    done.current = true;
+
+    const init = document.createElement("script");
+    init.text = INIT;
+    ref.current.appendChild(init);
+
+    const widget = document.createElement("script");
+    widget.id = "amoforms_script_1725042";
+    widget.async = true;
+    widget.charset = "utf-8";
+    widget.src = "https://forms.amocrm.ru/forms/assets/js/amoforms.js?1784264472";
+    ref.current.appendChild(widget);
+  }, []);
+
+  return <div ref={ref} className="w-full" />;
 }
